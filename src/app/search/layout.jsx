b8a28ea2dcdd102514c7c2c72442f09e450282d4
Sarnava Mohanta <sarnavamohanta2001@gmail.com>
2024-02-29 18:46:16 +0530

    update the search header and the web

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,14 @@
+import SearchHeader from "@/components/SearchHeader";
+import './../globals.css'
+
+
+import React from "react";
+
+export default function layout({ children }) {
+  return (
+    <div>
+      <SearchHeader />
+      {children}
+    </div>
+  );
+}
